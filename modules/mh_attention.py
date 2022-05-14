b9f23107d7b594cde3d25e4e20d9f0e25b58ec89
Torphix <66824187+torphix@users.noreply.github.com>
2022-05-14 09:19:30 +0100

    Update mh_attention.py

--- modules/mh_attention.py
+++ modules/mh_attention.py
@@ -3,41 +3,43 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class SoftmaxAttention(nn.Module):
     '''
     softmax(Q * K.T / d_k) * V
     '''
 
-    def __init__(self, d_k):
+    def __init__(self):
         super().__init__()
-        self.d_k = d_k
 
-    def forward(self, q, k, v, mask=None):
+    def forward(self, q, k, v, mask):
         '''
-        q, k, v are equal in encoder
-        q, k are encoder output in decoder
-        v is decoder embedding in decoder
-        mask is for padding
+        Encoder:
+            q, k, v are equal in encoder
+            mask is for padding
+        Decoder:
+            q is decoder output
+            k, v is encoder output
+            mask is padding + lookahead
         '''
-        attn = F.softmax((q @ k.transpose(1, 2)) / math.sqrt(self.d_k), dim=-1)
+        attn = F.softmax((q @ k.transpose(2,3)) / 
+                         math.sqrt(k.shape[-1]), dim=-1)
         if mask is not None:
-            attn.masked_fill(mask, -torch.inf)
+            attn = attn.masked_fill(mask==False, -torch.inf)
         out = attn @ v
         return out, attn
 
 
 class MultiHeadedAttention(nn.Module):
-    def __init__(self, in_d, d_q, d_k, d_v, n_heads):
+    def __init__(self, in_d, n_heads):
         super().__init__()
         self.n_heads = n_heads
 
-        self.k_linear = nn.Linear(in_d, n_heads*d_k)
-        self.v_linear = nn.Linear(in_d, n_heads*d_v)
-        self.q_linear = nn.Linear(in_d, n_heads*d_q)
+        self.k_linear = nn.Linear(in_d, n_heads*in_d)
+        self.v_linear = nn.Linear(in_d, n_heads*in_d)
+        self.q_linear = nn.Linear(in_d, n_heads*in_d)
 
-        self.attention = SoftmaxAttention(d_k)
-        self.out_layer = nn.Linear(n_heads*d_v, in_d)
+        self.attention = SoftmaxAttention()
+        self.out_layer = nn.Linear(n_heads*in_d, in_d)
 
         self.dropout = nn.Dropout(p=0.4)
         self.normalize = nn.LayerNorm(in_d)
@@ -51,7 +53,6 @@ class MultiHeadedAttention(nn.Module):
         N = N // self.n_heads
         x = x.view(BS, L, self.n_heads, N)
         x = x.permute(0, 2, 1, 3).contiguous()
-        x = x.view(BS * self.n_heads, L, N)
         return x
 
     def _map_from_heads(self, x):
@@ -59,18 +60,22 @@ class MultiHeadedAttention(nn.Module):
         x: [BS*Heads, L, N]
         out: [BS, L, N*Heads]
         '''
-        BS, L, N = x.size()
-        BS = BS // self.n_heads
+            
+        BS, H, L, N = x.size()
         x = x.view(BS, self.n_heads, L, N)
         x = x.permute(0, 2, 1, 3).contiguous()
         x = x.view(BS, L, N * self.n_heads)
         return x
 
-    def forward(self, q, k, v):
+    def forward(self, q, k, v, mask=None):
         '''
-        q, k, v are equal in encoder
-        q, k are encoder output in decoder
-        v is decoder embedding in decoder
+        Encoder:
+            q, k, v are equal in encoder
+            mask is for padding
+        Decoder:
+            q is decoder output
+            k, v is encoder output
+            mask is padding + lookahead
         '''
         q = self.q_linear(q)
         k = self.k_linear(k)
@@ -79,20 +84,16 @@ class MultiHeadedAttention(nn.Module):
         q = self._map_to_heads(q)
         k = self._map_to_heads(k)
         v = self._map_to_heads(v)
+        
+        if mask is not None:
+            # [BS, 1, Q_Len, K_len] -> [BS, N_heads, Q_len, K_len]
+            mask = mask.repeat(1, self.n_heads, 1, 1)
+        else: mask = None
 
-        qkv, attn = self.attention(q, k, v)
+        qkv, attn = self.attention(q, k, v, mask)
+        
         qkv = self._map_from_heads(qkv)
         out = self.out_layer(qkv)
         # Regularization
         out = self.normalize(self.dropout(out))
-        return out
-
-
-k, q, v = torch.randn((4, 57, 128)), torch.randn(
-    (4, 57, 128)), torch.randn((4, 57, 128))
-
-
-mha = MultiHeadedAttention(128, 256, 256, 256, 8)
-
-out = mha(q, k, v)
-print(out.size())
+        return out, attn
